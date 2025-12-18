18d1248767411d3aa159cf0eaf5cbb2ed9e4d22b
DESKTOP-ACFEFB8\USER <dahunj@gmail.com>
2025-12-18 19:47:29 +0900

    버전 수정

--- CMAI2100D/stdafx.h
+++ CMAI2100D/stdafx.h
@@ -75,7 +75,7 @@
 #endif
 
 //****************************************************************************
-//#define AJIN_BOARD_USE
+#define AJIN_BOARD_USE
 
 
 #define CARRIER_TYPE_MEM //CARRIER_OFFSETY=0
@@ -96,11 +96,11 @@
 #define PICK_CNT		8
 
 #ifdef CARRIER_TYPE_MEM
-	#define MAIN_VERSION	"Vd 3.1.15m"
+	#define MAIN_VERSION	"Vd 3.1.16m"
 	#define CARRIER_OFFSETX	8.0
 	#define CARRIER_OFFSETY	0.0
 #else
-	#define MAIN_VERSION	"Vd 3.1.15v"
+	#define MAIN_VERSION	"Vd 3.1.16v"
 	#define CARRIER_OFFSETX	8.0
 	#define CARRIER_OFFSETY	8.0
 #endif
